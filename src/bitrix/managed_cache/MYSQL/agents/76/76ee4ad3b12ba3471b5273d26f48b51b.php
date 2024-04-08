77aea6115366a11abec1fbbd1d6010eb61a360af
Daniil Gavrilov <dp070593gda@gmial.com>
2024-04-08 23:47:01 +0300

    Deploy script

--- src/bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ src/bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001712587742';
-$dateexpire = '001712591402';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1712587801;}';
+$datecreate = '001712607572';
+$dateexpire = '001712611232';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1712607632;}';
 return true;
 ?>
